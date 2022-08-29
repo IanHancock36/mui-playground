3e0624c0e1f438d1989bdb017b346d6657ab56e8
Ian Hancock <northroadian@gmail.com>
2022-08-29 17:29:47 -0500

    button centered

--- src/main-page.js
+++ src/main-page.js
@@ -10,6 +10,8 @@ import {
   Toolbar,
   Container
 } from '@mui/material';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import CameraEnhanceIcon from '@mui/icons-material/CameraEnhance';
 const MainPage = () => {
   return (
@@ -17,10 +19,29 @@ const MainPage = () => {
       <CssBaseline />
       <AppBar position='relative'>
         <Toolbar>
-          <CameraEnhanceIcon />
+          <CameraEnhanceIcon onClick={()=> console.log("CLICKED")}/>
           <Typography variant="h6">Travel Photos</Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              My Album
+            </Typography>
+            <Typography variant="h5" align='center' color="textSecondary" paragraph>
+              Hey! Thank you for taking a look at my photo album for my fictional travels. 
+            </Typography>
+            <Container align="center">
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+  <Button>One</Button>
+  <Button>Two</Button>
+  <Button>Three</Button>
+</ButtonGroup>
+            </Container>
+          </Container>
+        </div>
+      </main>
     </>
   )
 }
