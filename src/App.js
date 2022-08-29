720cfb9b493273cad4bee15dce083166d85403b1
Ian Hancock <northroadian@gmail.com>
2022-08-29 16:13:12 -0500

    Appbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import ErrorScreen from './pages/error-screen';
+import MainPage from './main-page';
 
 export default function App() {
 return (
--- src/main-page.js
+++ src/main-page.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
-
-const MainPage = () =>{
-    return (
-        <div>HEY </div>
-    )
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline, Grid,
+  Toolbar,
+  Container
+} from '@mui/material';
+import CameraEnhanceIcon from '@mui/icons-material/CameraEnhance';
+const MainPage = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <CameraEnhanceIcon />
+          <Typography variant="h6">Travel Photos</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
 }
 
 export default MainPage
