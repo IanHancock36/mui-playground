f64f9ebbad406990b24e9846be62ba783f660961
Ian Hancock <northroadian@gmail.com>
2022-08-31 14:23:51 -0500

    folder change prettier

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
 /** @jsxImportSource @emotion/react */
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Typography,
   AppBar,
@@ -8,12 +8,12 @@ import {
   Toolbar,
   Container,
   Grid,
-} from '@mui/material';
-import { css } from '@emotion/react';
-import MediaCard from '../components/media-card'
-import SearchBar from '../components/search-bar'
-import { TravelExplore } from '@mui/icons-material';
-import teamData from '../db/MOCK_DATA.json'
+} from "@mui/material";
+import { css } from "@emotion/react";
+import MediaCard from "../components/MediaCard";
+import SearchBar from "../components/SearchBar";
+import { TravelExplore } from "@mui/icons-material";
+import teamData from "../db/MOCK_DATA.json";
 
 const filterTeamMembers = (teamData, searchedTeamMembers) => {
   if (!searchedTeamMembers) {
@@ -31,19 +31,46 @@ const MainPage = () => {
   return (
     <>
       <CssBaseline />
-      <AppBar position='relative'>
-        <Toolbar css={css`background-color: #2e8b57; color: #fff;`}>
+      <AppBar position="relative">
+        <Toolbar
+          css={css`
+            background-color: #2e8b57;
+            color: #fff;
+          `}
+        >
           <TravelExplore onClick={() => console.log("CLICKED")} />
-          <Typography variant="h6" css={css`margin-left:5px`}>Mock Company</Typography>
+          <Typography
+            variant="h6"
+            css={css`
+              margin-left: 5px;
+            `}
+          >
+            Mock Company
+          </Typography>
         </Toolbar>
       </AppBar>
       <main>
         <div>
-          <Container maxWidth="sm" css={css`margin-top:100px`}>
-            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+          <Container
+            maxWidth="sm"
+            css={css`
+              margin-top: 100px;
+            `}
+          >
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Company Directory
             </Typography>
-            <Typography variant="h5" align='center' color="textSecondary" paragraph>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
               Search Our Wonderful Team Members
             </Typography>
             <div>
@@ -57,9 +84,14 @@ const MainPage = () => {
           </Container>
         </div>
         <div>
-          <Container maxWidth="lg" css={css`margin-top:100px`} >
-            <Grid container spacing={4} align='center'>
-              {filteredTeamData.map(cardInfo => (
+          <Container
+            maxWidth="lg"
+            css={css`
+              margin-top: 100px;
+            `}
+          >
+            <Grid container spacing={4} align="center">
+              {filteredTeamData.map((cardInfo) => (
                 <Grid item key={cardInfo.id} xs={12} sm={6} md={4}>
                   <MediaCard cardInfo={cardInfo} />
                 </Grid>
@@ -69,7 +101,7 @@ const MainPage = () => {
         </div>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
